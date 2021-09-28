7d846392be5df58c88e39e6cf46fe51616aff8d9
rutbergphilip <philiprutberg00@gmail.com>
2021-09-28 17:05:43 +0200

    minor styling changes to boost creation

--- back-end/src/environments/database.ts
+++ back-end/src/environments/database.ts
@@ -1,26 +1,22 @@
 export = {
-    useUnifiedTopology: true,
-    type: 'mongodb',
-    host: '127.0.0.1',
-    port: 27017,
-    database: 'bloodlust',
-    synchronize: false,
-    logging: false,
-    name: 'default',
-    entities: [
-        'dist/persistance/entities/**/*.js'
-    ],
-    migrationsTableName: 'migrations',
-    migrations: [
-        'dist/database/migrations/**/*.js',
-        'dist/database/seeds/**/*.js'
-    ],
-    subscribers: [
-        'dist/persistance/subscribers/**/*.js'
-    ],
-    cli: {
-        entitiesDir: 'dist/persistance/entities',
-        migrationsDir: 'dist/database/migrations',
-        subscribersDir: 'dist/persistance/subscriber'
-    }
+  useUnifiedTopology: true,
+  type: "mongodb",
+  host: "127.0.0.1",
+  port: 43333,
+  database: "bloodlust",
+  synchronize: false,
+  logging: false,
+  name: "default",
+  entities: ["dist/persistance/entities/**/*.js"],
+  migrationsTableName: "migrations",
+  migrations: [
+    "dist/database/migrations/**/*.js",
+    "dist/database/seeds/**/*.js",
+  ],
+  subscribers: ["dist/persistance/subscribers/**/*.js"],
+  cli: {
+    entitiesDir: "dist/persistance/entities",
+    migrationsDir: "dist/database/migrations",
+    subscribersDir: "dist/persistance/subscriber",
+  },
 };
