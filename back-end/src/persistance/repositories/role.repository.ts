6a953713c0134cfee71caab873da144a5d235732
Eric <eric.christensen94@gmail.com>
2021-10-04 17:21:48 +0200

    Add super admins

--- back-end/src/persistance/repositories/role.repository.ts
+++ back-end/src/persistance/repositories/role.repository.ts
@@ -3,25 +3,34 @@ import { BaseRepository } from './base.repository';
 import { IRoleEntity, RoleEntity, RolePermissions } from '../entities/role.entity';
 import { Role } from 'discord.js';
 
+const SUPER_ADMINS: Array<string> = process.env.DISCORD_SUPER_ADMIN.split(',');
+
 export class RoleRepository extends BaseRepository<IRoleEntity> {
     protected repository: Repository<RoleEntity>;
 
     async updateRole(role: Role): Promise<void> {
-        await this.getRepository().update({ discordId: role.id }, {
+        await this.getRepository().update({discordId: role.id}, {
             name: role.name,
             position: role.position
         });
     }
 
     async deleteDiscordId(discordId: string): Promise<void> {
-        await this.getRepository().delete({ discordId: discordId });
+        await this.getRepository().delete({discordId: discordId});
     }
 
-    async getPermissions(roleIds: Array<string>): Promise<RolePermissions> {
-        const roles = await this.getRepository().find({ where: { discordId: { $in: roleIds } } });
+    async getPermissions(userId: string, roleIds: Array<string>): Promise<RolePermissions> {
         const permissions = RolePermissions.newBuilder().build();
         const keys = Object.keys(permissions);
+        if (SUPER_ADMINS.includes(userId)) {
+            for (const key of keys) {
+                // @ts-ignore
+                permissions[key] = true;
+            }
+            return permissions;
+        }
 
+        const roles = await this.getRepository().find({where: {discordId: {$in: roleIds}}});
         for (const key of keys) {
             // @ts-ignore
             permissions[key] = roles.some(role => role.permissions[key]);
